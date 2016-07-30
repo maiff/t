d87bd5d103c64dc979a6acbbc3e4d4d3fa3b49af
maiff <864306867@qq.com>
2016-07-30 22:34:07 +0800

    2.0

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ var fs=require('fs');
 var path=require('path');
 
 exports=module.exports=function bulid(inputPath,outputPath){
-	var data=dealWithKeyWord(readFile(inputPath),inputPath);
-	fs.writeFileSync(outputPath, data);
+    var data=dealWithKeyWord(readFile(inputPath),inputPath);
+    fs.writeFileSync(outputPath, data);
 };
 
 var mainStr='';
 
 
 function readFile(inputPath){
-	return fs.readFileSync(inputPath, {
-		encoding:'UTF-8'
-	});
+    return fs.readFileSync(inputPath, {
+        encoding:'UTF-8'
+    });
 }
 
 function dealWithKeyWord(str,dirname){
-	var reg=/<%include ([\w]+) %>/g;
+    var reg=/<%include (\w+)(\{\S+\}) %>/g;
 
-	return str.replace(reg,function(m,p1){
-		
-		return readFile(path.join(path.dirname(dirname),p1+'.ttt'));
-	});
+    var reg2=/\$\{(\w+)\}/g;
+    
+    var one_str=str.replace(reg,function(m,p1,p2){
+        var obj=JSON.parse(p2);//获取对象
+        var getStr=readFile(path.join(path.dirname(dirname),p1+'.ttt'));
+        return getStr.replace(reg2,function(m,p1){
+            return obj[p1];
+        })
+    });
 
 
 }
