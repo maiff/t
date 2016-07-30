35ab4320085c55b6197778902ec924a72282ec29
maiff <864306867@qq.com>
2016-07-30 23:19:33 +0800

    2.0 add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs=require('fs');
 var path=require('path');
 
 exports=module.exports=function bulid(inputPath,outputPath){
-    var data=dealWithKeyWord(readFile(inputPath),inputPath);
+    var data=getInputObjAndRplaceByRightContent(readFile(inputPath),inputPath);
     fs.writeFileSync(outputPath, data);
 };
 
@@ -15,20 +15,26 @@ function readFile(inputPath){
     });
 }
 
-function dealWithKeyWord(str,dirname){
-    var reg=/<%include (\w+)(\{\S+\}) %>/g;
-
-    var reg2=/\$\{(\w+)\}/g;
-    
-    var one_str=str.replace(reg,function(m,p1,p2){
-        var obj=JSON.parse(p2);//获取对象
-        var getStr=readFile(path.join(path.dirname(dirname),p1+'.ttt'));
-        return getStr.replace(reg2,function(m,p1){
-            return obj[p1];
-        })
-    });
 
 
+function getInputObjAndRplaceByRightContent(str,dirname){
+	var reg=/<%include (\w+)(\{[^0]+\})* %>/g;
+	
+	return str.replace(reg,function(m,p1,p2){
+		 var obj=eval('('+p2+')');//获取对象
+		 var TStr=readFile(path.join(path.dirname(dirname),p1+'.ttt'));
+		 return getTTTObjWordAndRplaceByContent(TStr,obj);
+	});
 }
 
-//console.log(dealWithKeyWord(readFile('test.html')));
+function getTTTObjWordAndRplaceByContent(Tstr,obj){
+	var reg2=/\$\{(\w+)\}/g;
+	return Tstr.replace(reg2,function(m,p1){
+        	//console.log(p1)
+            return obj[p1];
+        });
+}
+
+
+
+
